cde0a41a8b9e31f6300906dfa661f8d60cc7f268
정승훈 <hks026naver@gmail.com>
2023-09-15 19:17:04 +0900

    ♻️ :: ktlint

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.signal.signal_android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.signal.signal_android"
