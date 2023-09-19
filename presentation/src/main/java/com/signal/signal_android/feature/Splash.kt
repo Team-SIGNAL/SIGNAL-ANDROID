b93df9394ab472e3067191825624cc60e4248566
정승훈 <hks026naver@gmail.com>
2023-09-19 17:31:35 +0900

    ⚡️ :: Splash setting

---
+++ presentation/src/main/java/com/signal/signal_android/feature/Splash.kt
@@ -0,0 +1,13 @@
+package com.signal.signal_android.feature
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun Splash() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
--- presentation/src/main/java/com/signal/signal_android/navigation/UserNavigation.kt
+++ presentation/src/main/java/com/signal/signal_android/navigation/UserNavigation.kt
@@ -3,14 +3,15 @@ package com.signal.signal_android.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.signal.signal_android.feature.Splash
 
 internal fun NavGraphBuilder.userNavigation() {
     navigation(
         route = NavigationRoute.User.route,
         startDestination = NavigationRoute.User.Splash,
-    ){
-        composable(NavigationRoute.User.Splash){
-
+    ) {
+        composable(NavigationRoute.User.Splash) {
+            Splash()
         }
     }
 }
