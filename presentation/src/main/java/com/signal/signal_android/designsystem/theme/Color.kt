6d3bfe27337a72de4f11ee5dbe931f5b7b128afd
정승훈 <hks026naver@gmail.com>
2023-09-21 15:07:36 +0900

    ⚡️ :: Signal Color

--- presentation/src/main/java/com/signal/signal_android/designsystem/theme/Color.kt
+++ presentation/src/main/java/com/signal/signal_android/designsystem/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.signal.signal_android.designsystem.theme
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,42 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object SignalColor {
+
+    @Stable
+    val Primary100 = Color(0xFF00E252)
+
+    @Stable
+    val Primary200 = Color(0xFF43CB74)
+
+    @Stable
+    val Primary300 = Color(0xFF3CB066)
+
+    @Stable
+    val Primary400 = Color(0xFF529959)
+
+    @Stable
+    val Primary500 = Color(0xFF4E8C55)
+
+    @Stable
+    val Gray100 = Color(0xFFFFFFFF)
+
+    @Stable
+    val Gray200 = Color(0xFFF7F7F7)
+
+    @Stable
+    val Gray300 = Color(0xFFE5E5E5)
+
+    @Stable
+    val Gray400 = Color(0xFFCCCCCC)
+
+    @Stable
+    val Gray500 = Color(0xFF7F7F7F)
+
+    @Stable
+    val Gray600 = Color(0xFF444444)
+
+    @Stable
+    val Gray700 = Color(0xFF333333)
+}
