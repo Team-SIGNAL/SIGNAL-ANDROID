3f633ba622d9682d8b3d89c6a78bf35b2df7518e
정승훈 <hks026naver@gmail.com>
2023-09-24 14:18:59 +0900

    ⚡️ :: Indicator preview

--- presentation/src/main/java/com/signal/signal_android/designsystem/component/Indicator.kt
+++ presentation/src/main/java/com/signal/signal_android/designsystem/component/Indicator.kt
@@ -5,19 +5,38 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.signal.signal_android.R
 import com.signal.signal_android.designsystem.foundation.SignalColor
 
 private const val DURATION = 500
+
+@Preview(showBackground = true)
+@Composable
+fun IndicatorPreview() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Indicator(isEnabled = true)
+        Indicator(isEnabled = false)
+        Indicator(isEnabled = false)
+        Indicator(isEnabled = false)
+        Indicator(isEnabled = false)
+    }
+}
 
 @Composable
 fun Indicator(isEnabled: Boolean) {
