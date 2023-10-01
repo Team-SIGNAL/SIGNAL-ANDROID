5e9b5f9491a99be0e808db2cd5e1a3cfb3b96efc
정승훈 <hks026naver@gmail.com>
2023-10-01 19:42:19 +0900

    ⚡️ :: base view model

---
+++ presentation/src/main/java/com/signal/signal_android/viewmodel/BaseViewModel.kt
@@ -0,0 +1,23 @@
+package com.signal.signal_android.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+abstract class BaseViewModel<S, E>(initialState: S) : ViewModel() {
+    private val _state: MutableStateFlow<S> = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    private val _sideEffect: MutableSharedFlow<E> = MutableSharedFlow()
+    val sideEffect = _sideEffect.asSharedFlow()
+
+    protected fun setState(state: S) {
+        _state.tryEmit(state)
+    }
+
+    protected fun postSideEffect(sideEffect: E) {
+        _sideEffect.tryEmit(sideEffect)
+    }
+}
