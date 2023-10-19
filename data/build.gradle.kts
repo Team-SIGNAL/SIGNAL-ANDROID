ddca2796df911ee1e25c0845cb219582a2d50d6c
Tmdhoon2 <hks026naver@gmail.com>
2023-10-20 08:36:58 +0900

    ⚙️ :: logging interceptor

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     api("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
