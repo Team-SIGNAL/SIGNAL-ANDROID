682467646f306e63a3acd3bbfc8c766f6828873b
chlgkdms <haeun3882@gmail.com>
2023-10-25 12:16:49 +0900

    ⚡️ :: MyPage Koin 연결

--- presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
+++ presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
@@ -9,59 +9,76 @@ import com.signal.data.datasource.user.remote.RemoteUserDataSourceImpl
 import com.signal.data.repository.UserRepositoryImpl
 import com.signal.data.util.TokenInterceptor
 import com.signal.domain.repository.UserRepository
+import com.signal.domain.usecase.users.SecessionUseCase
 import com.signal.domain.usecase.users.SignInUseCase
+import com.signal.domain.usecase.users.SignOutUseCase
 import com.signal.domain.usecase.users.SignUpUseCase
+import com.signal.signal_android.feature.mypage.MyPageViewModel
 import com.signal.signal_android.feature.signup.SignUpViewModel
 import com.signal.signal_android.viewmodel.SignInViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
 class SignalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val apiModule = module {
-            single { TokenInterceptor(get()) }
-            single { ApiProvider.getUserApi(get()) }
-        }
-
-        val dataSourceModule = module {
-            single<RemoteUserDataSource> { RemoteUserDataSourceImpl(get()) }
-            single<LocalUserDataSource> { LocalUserDataSourceImpl(androidContext()) }
-        }
-
-        val repositoryModule = module {
-            single<UserRepository> {
-                UserRepositoryImpl(
-                    remoteUserDataSource = get(),
-                    localUserDataSource = get(),
-                )
-            }
-        }
-
-        val useCaseModule = module {
-            single { SignInUseCase(get()) }
-            single { SignUpUseCase(get()) }
-        }
-
-        val viewModelModule = module {
-            viewModel { SignInViewModel(get()) }
-            viewModel { SignUpViewModel(get()) }
-        }
-
         startKoin {
-            androidContext(applicationContext)
-            modules(
-                listOf(
-                    apiModule,
-                    dataSourceModule,
-                    repositoryModule,
-                    useCaseModule,
-                    viewModelModule,
-                ),
-            )
+            androidContext(this@SignalApplication)
+            modules(signalModule)
         }
     }
 }
+
+val signalModule: Module
+    get() = module {
+        includes(
+            apiModule,
+            dataSourceModule,
+            repositoryModule,
+            useCaseModule,
+            viewModelModule,
+        )
+    }
+
+val apiModule: Module
+    get() = module {
+        single { TokenInterceptor(get()) }
+        single { ApiProvider.getUserApi(get()) }
+    }
+
+val dataSourceModule: Module
+    get() = module {
+        single<RemoteUserDataSource> { RemoteUserDataSourceImpl(get()) }
+        single<LocalUserDataSource> { LocalUserDataSourceImpl(androidContext()) }
+    }
+
+val repositoryModule: Module
+    get() = module {
+        single<UserRepository> {
+            UserRepositoryImpl(
+                remoteUserDataSource = get(),
+                localUserDataSource = get(),
+            )
+        }
+    }
+
+val useCaseModule: Module
+    get() = module {
+        single { SignInUseCase(get()) }
+        single { SignUpUseCase(get()) }
+        single { SignOutUseCase(get()) }
+        single { SecessionUseCase(get()) }
+    }
+
+val viewModelModule: Module
+    get() = module {
+        viewModel { SignInViewModel(get()) }
+        viewModel { SignUpViewModel(get()) }
+        viewModel {
+            MyPageViewModel(get(), get())
+        }
+    }
