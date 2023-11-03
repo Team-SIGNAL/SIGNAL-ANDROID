0400516a9e513ca5130d6f62ff7ff9ec88ad08bb
정승훈 <hks026naver@gmail.com>
2023-11-03 20:26:53 +0900

    ⚡️ :: 뒤로가기 로직 구현

--- presentation/src/main/java/com/signal/signal_android/MainActivity.kt
+++ presentation/src/main/java/com/signal/signal_android/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.signal.signal_android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.signal.signal_android.designsystem.foundation.SignalAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var delay = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
+            BackHandler(onBack = this::setBackHandler)
             SignalAndroidTheme {
                 SignalApp()
             }
+        }
+    }
+
+    private fun setBackHandler() {
+        if (System.currentTimeMillis() > delay) {
+            delay = System.currentTimeMillis() + 1000
+            Toast.makeText(this, getText(R.string.back_handler_message), Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
         }
     }
 }
