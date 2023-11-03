983dfb76c0e6af30b1bdab96c267e679297cc96f
chlgkdms <haeun3882@gmail.com>
2023-11-03 15:56:24 +0900

    ⚡️ :: Diary 퍼블리싱 및 string 추가

---
+++ presentation/src/main/java/com/signal/signal_android/feature/main/diary/CreateDiary.kt
@@ -0,0 +1,19 @@
+package com.signal.signal_android.feature.main.diary
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun CreateDiary() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
+    }
+}
+
