4cae3a2856a00e1251948386f93fcb364c43027a
chlgkdms <haeun3882@gmail.com>
2023-11-12 22:33:33 +0900

    ⚡️ :: DiaryDataSource 구현

--- data/src/main/kotlin/com/signal/data/datasource/diary/DiaryDataSource.kt
+++ data/src/main/kotlin/com/signal/data/datasource/diary/DiaryDataSource.kt
@@ -1,7 +1,14 @@
 package com.signal.data.datasource.diary
 
 import com.signal.data.model.diary.CreateDiaryRequest
+import com.signal.data.model.diary.FetchAllDiaryResponse
+import com.signal.data.model.diary.FetchDayDiaryResponse
+import com.signal.data.model.diary.FetchMonthDiaryResponse
+import java.time.LocalDateTime
 
 interface DiaryDataSource {
     suspend fun createDiary(createDiaryRequest: CreateDiaryRequest)
+    suspend fun fetchAllDiary(): FetchAllDiaryResponse
+    suspend fun fetchMonthDiary(date: LocalDateTime): FetchMonthDiaryResponse
+    suspend fun fetchDayDiary(date: LocalDateTime): FetchDayDiaryResponse
 }
--- data/src/main/kotlin/com/signal/data/datasource/diary/DiaryDateSourceImpl.kt
+++ data/src/main/kotlin/com/signal/data/datasource/diary/DiaryDateSourceImpl.kt
@@ -2,13 +2,33 @@ package com.signal.data.datasource.diary
 
 import com.signal.data.api.DiaryApi
 import com.signal.data.model.diary.CreateDiaryRequest
+import com.signal.data.model.diary.FetchAllDiaryResponse
+import com.signal.data.model.diary.FetchDayDiaryResponse
+import com.signal.data.model.diary.FetchMonthDiaryResponse
 import com.signal.data.util.ExceptionHandler
+import java.time.LocalDateTime
 
 class DiaryDateSourceImpl(
     private val diaryApi: DiaryApi,
 ) : DiaryDataSource {
     override suspend fun createDiary(createDiaryRequest: CreateDiaryRequest) =
         ExceptionHandler<Unit>().httpRequest {
-            diaryApi.createDiary(createDiaryRequest)
+            diaryApi.createDiary(createDiaryRequest = createDiaryRequest)
         }.sendRequest()
+
+    override suspend fun fetchAllDiary(): FetchAllDiaryResponse =
+        ExceptionHandler<FetchAllDiaryResponse>().httpRequest {
+            diaryApi.fetchAllDiary()
+        }.sendRequest()
+
+    override suspend fun fetchMonthDiary(date: LocalDateTime): FetchMonthDiaryResponse =
+        ExceptionHandler<FetchMonthDiaryResponse>().httpRequest {
+            diaryApi.fetchMonthDiary(date = date)
+        }.sendRequest()
+
+    override suspend fun fetchDayDiary(date: LocalDateTime): FetchDayDiaryResponse =
+        ExceptionHandler<FetchDayDiaryResponse>().httpRequest {
+            diaryApi.fetchDayDiary(date = date)
+        }.sendRequest()
+
 }
