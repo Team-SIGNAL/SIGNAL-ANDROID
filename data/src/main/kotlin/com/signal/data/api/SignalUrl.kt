02d4afe21b397b62f68d2572a5059a07681486a6
chlgkdms <haeun3882@gmail.com>
2023-11-14 20:02:49 +0900

    ♻️ :: url 수정

--- data/src/main/kotlin/com/signal/data/api/SignalUrl.kt
+++ data/src/main/kotlin/com/signal/data/api/SignalUrl.kt
@@ -27,7 +27,6 @@ object SignalUrl {
     object Diary {
         const val CreateDiary = diary
         const val FetchAllDiary = "$diary/list"
-        const val FetchDayDiary = diary
         const val FetchMonthDiary = "$diary/month"
     }
 }
