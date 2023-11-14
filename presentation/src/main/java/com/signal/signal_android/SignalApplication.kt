a9da6160ecd992551f2c2199a414165a09e8cc20
chlgkdms <haeun3882@gmail.com>
2023-11-14 20:01:59 +0900

    ⚡️ :: koin 추가

--- presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
+++ presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
@@ -6,6 +6,8 @@ import com.signal.data.api.ApiProvider
 import com.signal.data.database.SignalDatabase
 import com.signal.data.datasource.diagnosis.LocalDiagnosisDataSource
 import com.signal.data.datasource.diagnosis.LocalDiagnosisDataSourceImpl
+import com.signal.data.datasource.diary.DiaryDataSource
+import com.signal.data.datasource.diary.DiaryDataSourceImpl
 import com.signal.data.datasource.feed.FeedDataSource
 import com.signal.data.datasource.feed.FeedDataSourceImpl
 import com.signal.data.datasource.file.AttachmentDataSource
@@ -16,12 +18,14 @@ import com.signal.data.datasource.user.remote.RemoteUserDataSource
 import com.signal.data.datasource.user.remote.RemoteUserDataSourceImpl
 import com.signal.data.repository.AttachmentRepositoryImpl
 import com.signal.data.repository.DiagnosisRepositoryImpl
+import com.signal.data.repository.DiaryRepositoryImpl
 import com.signal.data.repository.FeedRepositoryImpl
 import com.signal.data.repository.UserRepositoryImpl
 import com.signal.data.util.PreferenceManager
 import com.signal.data.util.TokenInterceptor
 import com.signal.domain.repository.AttachmentRepository
 import com.signal.domain.repository.DiagnosisRepository
+import com.signal.domain.repository.DiaryRepository
 import com.signal.domain.repository.FeedRepository
 import com.signal.domain.repository.UserRepository
 import com.signal.domain.usecase.users.FetchUserInformationUseCase
@@ -33,6 +37,7 @@ import com.signal.domain.usecase.users.SignOutUseCase
 import com.signal.domain.usecase.users.SignUpUseCase
 import com.signal.signal_android.feature.diagnosis.DiagnosisViewModel
 import com.signal.signal_android.feature.file.AttachmentViewModel
+import com.signal.signal_android.feature.main.diary.DiaryViewModel
 import com.signal.signal_android.feature.main.feed.FeedViewModel
 import com.signal.signal_android.feature.main.home.HomeViewModel
 import com.signal.signal_android.feature.mypage.MyPageViewModel
@@ -73,6 +78,7 @@ val apiModule: Module
         single { ApiProvider.getUserApi(tokenInterceptor = get()) }
         single { ApiProvider.getFeedApi(tokenInterceptor = get()) }
         single { ApiProvider.getFileApi(tokenInterceptor = get()) }
+        single { ApiProvider.getDiaryApi(tokenInterceptor = get()) }
     }
 
 val daoModule: Module
@@ -107,6 +113,7 @@ val dataSourceModule: Module
                 preferenceManager = get(),
             )
         }
+        single<DiaryDataSource> { DiaryDataSourceImpl(diaryApi = get()) }
     }
 
 val repositoryModule: Module
@@ -125,6 +132,9 @@ val repositoryModule: Module
         }
         single<DiagnosisRepository> {
             DiagnosisRepositoryImpl(localDiagnosisDataSource = get())
+        }
+        single<DiaryRepository> {
+            DiaryRepositoryImpl(diaryDateSource = get())
         }
     }
 
@@ -164,4 +174,5 @@ val viewModelModule: Module
             )
         }
         viewModel { HomeViewModel(diagnosisRepository = get()) }
+        viewModel { DiaryViewModel(diaryRepository = get()) }
     }
