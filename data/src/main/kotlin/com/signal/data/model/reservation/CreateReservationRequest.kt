426a45c3d0271c6828f0e4b3889f319f8da34b2b
chlgkdms <haeun3882@gmail.com>
2023-11-17 19:42:15 +0900

    ⚡️ :: Reservation Request 추가

---
+++ data/src/main/kotlin/com/signal/data/model/reservation/CreateReservationRequest.kt
@@ -0,0 +1,8 @@
+package com.signal.data.model.reservation
+
+import com.google.gson.annotations.SerializedName
+
+data class CreateReservationRequest(
+    @SerializedName("reason") val reason: String,
+    @SerializedName("date") val date: String,
+)
