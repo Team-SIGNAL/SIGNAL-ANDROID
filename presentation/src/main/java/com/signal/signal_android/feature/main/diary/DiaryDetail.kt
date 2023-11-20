ac2247b61fa563f0a2316bee4b1ffe854482382e
chlgkdms <haeun3882@gmail.com>
2023-11-20 23:50:52 +0900

    ♻️ :: 트레일링 컴마 추가

--- presentation/src/main/java/com/signal/signal_android/feature/main/diary/DiaryDetail.kt
+++ presentation/src/main/java/com/signal/signal_android/feature/main/diary/DiaryDetail.kt
@@ -10,30 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
-import com.signal.domain.enums.Emotion.*
 import com.signal.signal_android.R
 import com.signal.signal_android.designsystem.component.Header
-import com.signal.signal_android.designsystem.component.SignalDialog
 import com.signal.signal_android.designsystem.foundation.Body
 import com.signal.signal_android.designsystem.foundation.Body2
 import com.signal.signal_android.designsystem.foundation.BodyLarge2
@@ -57,7 +45,7 @@ internal fun DiaryDetail(
 
     LaunchedEffect(Unit) {
         diaryViewModel.sideEffect.collect {
-            when(it) {
+            when (it) {
                 DiarySideEffect.DeleteSuccess -> moveToBack()
                 else -> {}
             }
@@ -70,10 +58,12 @@ internal fun DiaryDetail(
             .fillMaxSize()
             .padding(horizontal = 16.dp)
     ) {
-        Header(title = stringResource(id = R.string.header_back),
+        Header(
+            title = stringResource(id = R.string.header_back),
             onLeadingClicked = moveToBack,
             trailingIcon = painterResource(id = R.drawable.ic_delete),
-            onTrailingClicked = { diaryViewModel.deleteDiary() })
+            onTrailingClicked = { diaryViewModel.deleteDiary() },
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
