7b7ce9be9e6e20227c442e4ef4c82cb66b8df1c8
chlgkdms <haeun3882@gmail.com>
2023-11-20 23:46:26 +0900

    ♻️ :: Long -> UUID

--- data/src/main/kotlin/com/signal/data/model/reservation/FetchHospitalsResponse.kt
+++ data/src/main/kotlin/com/signal/data/model/reservation/FetchHospitalsResponse.kt
@@ -2,13 +2,15 @@ package com.signal.data.model.reservation
 
 import com.google.gson.annotations.SerializedName
 import com.signal.domain.entity.FetchHospitalsEntity
+import java.util.UUID
 
 data class FetchHospitalsResponse(
     @SerializedName("hospital_list") val hospitals: List<Hospitals>
 
 ) {
     data class Hospitals(
-        @SerializedName("image") val image: String,
+        @SerializedName("id") val id: UUID,
+        @SerializedName("profile") val profile: String,
         @SerializedName("name") val name: String,
         @SerializedName("phone") val phone: String,
         @SerializedName("address") val address: String,
@@ -19,7 +21,8 @@ fun FetchHospitalsResponse.toEntity() =
     FetchHospitalsEntity(hospitals = this.hospitals.map { it.toEntity() })
 
 private fun FetchHospitalsResponse.Hospitals.toEntity() = FetchHospitalsEntity.HospitalsEntity(
-    image = this.image,
+    id = this.id,
+    profile = this.profile,
     name = this.name,
     phone = this.phone,
     address = this.address,
--- domain/src/main/kotlin/com/signal/domain/entity/FetchHospitalsEntity.kt
+++ domain/src/main/kotlin/com/signal/domain/entity/FetchHospitalsEntity.kt
@@ -1,10 +1,13 @@
 package com.signal.domain.entity
 
+import java.util.UUID
+
 data class FetchHospitalsEntity(
     val hospitals: List<HospitalsEntity>
 ) {
     data class HospitalsEntity(
-        val image: String?,
+        val id: UUID,
+        val profile: String?,
         val name: String,
         val phone: String,
         val address: String,
