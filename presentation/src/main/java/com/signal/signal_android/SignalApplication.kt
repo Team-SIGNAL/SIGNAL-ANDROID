fa33051bbbd6781a5d94d6e3d0b456c000d510f1
chlgkdms <haeun3882@gmail.com>
2023-11-22 12:05:00 +0900

    ♻️ :: viewModel import 추가

--- presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
+++ presentation/src/main/java/com/signal/signal_android/SignalApplication.kt
@@ -47,7 +47,7 @@ import com.signal.signal_android.feature.file.AttachmentViewModel
 import com.signal.signal_android.feature.main.diary.DiaryViewModel
 import com.signal.signal_android.feature.main.feed.FeedViewModel
 import com.signal.signal_android.feature.main.home.HomeViewModel
-import com.signal.signal_android.feature.main.mypage.MyPageViewModel
+import com.signal.signal_android.feature.mypage.MyPageViewModel
 import com.signal.signal_android.feature.reservation.ReservationViewModel
 import com.signal.signal_android.feature.signin.SignInViewModel
 import com.signal.signal_android.feature.signup.SignUpViewModel
