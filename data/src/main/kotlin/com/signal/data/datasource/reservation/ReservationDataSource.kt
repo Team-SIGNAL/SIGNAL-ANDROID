fb87a355b4780d73fd686d853c5d41671dad379c
chlgkdms <haeun3882@gmail.com>
2023-11-22 12:02:29 +0900

    ♻️ :: DataSource 수정

--- data/src/main/kotlin/com/signal/data/datasource/reservation/ReservationDataSource.kt
+++ data/src/main/kotlin/com/signal/data/datasource/reservation/ReservationDataSource.kt
@@ -4,10 +4,14 @@ import com.signal.data.model.reservation.CreateReservationRequest
 import com.signal.data.model.reservation.FetchDayReservationsResponse
 import com.signal.data.model.reservation.FetchHospitalsResponse
 import com.signal.data.model.reservation.FetchReservationDetailsResponse
+import java.util.UUID
 
 interface ReservationDataSource {
-    suspend fun createReservation(createReservationRequest: CreateReservationRequest)
+    suspend fun createReservation(
+        hospitalId: UUID,
+        createReservationRequest: CreateReservationRequest
+    )
     suspend fun fetchDayReservations(date: String): FetchDayReservationsResponse
     suspend fun fetchHospitals(): FetchHospitalsResponse
-    suspend fun fetchReservationDetails(reservationId: Long): FetchReservationDetailsResponse
+    suspend fun fetchReservationDetails(reservationId: UUID): FetchReservationDetailsResponse
 }
--- data/src/main/kotlin/com/signal/data/datasource/reservation/ReservationDataSourceImpl.kt
+++ data/src/main/kotlin/com/signal/data/datasource/reservation/ReservationDataSourceImpl.kt
@@ -6,14 +6,19 @@ import com.signal.data.model.reservation.FetchDayReservationsResponse
 import com.signal.data.model.reservation.FetchHospitalsResponse
 import com.signal.data.model.reservation.FetchReservationDetailsResponse
 import com.signal.data.util.ExceptionHandler
+import java.util.UUID
 
 class ReservationDataSourceImpl(
     private val reservationApi: ReservationApi,
 ) : ReservationDataSource {
-    override suspend fun createReservation(createReservationRequest: CreateReservationRequest) =
-        ExceptionHandler<Unit>().httpRequest {
-            reservationApi.createReservation(createReservationRequest = createReservationRequest)
-        }.sendRequest()
+    override suspend fun createReservation(
+        hospitalId: UUID, createReservationRequest: CreateReservationRequest
+    ) = ExceptionHandler<Unit>().httpRequest {
+        reservationApi.createReservation(
+            hospitalId = hospitalId,
+            createReservationRequest = createReservationRequest,
+        )
+    }.sendRequest()
 
     override suspend fun fetchDayReservations(date: String) =
         ExceptionHandler<FetchDayReservationsResponse>().httpRequest {
@@ -24,7 +29,7 @@ class ReservationDataSourceImpl(
         reservationApi.fetchHospitals()
     }.sendRequest()
 
-    override suspend fun fetchReservationDetails(reservationId: Long) =
+    override suspend fun fetchReservationDetails(reservationId: UUID) =
         ExceptionHandler<FetchReservationDetailsResponse>().httpRequest {
             reservationApi.fetchReservationDetails(reservationId = reservationId)
         }.sendRequest()
