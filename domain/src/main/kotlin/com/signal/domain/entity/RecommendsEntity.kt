e56208fe7c77daa50a34a8776e487136b23fd7a1
정승훈 <hks026naver@gmail.com>
2023-11-23 07:32:31 +0900

    ⚡️ :: 추천 리스트 보기 도메인 로직

---
+++ domain/src/main/kotlin/com/signal/domain/entity/RecommendsEntity.kt
@@ -0,0 +1,14 @@
+package com.signal.domain.entity
+
+import java.util.UUID
+
+data class RecommendsEntity(
+    val recommends: List<Recommend>,
+) {
+    data class Recommend(
+        val id: UUID,
+        val title: String,
+        val content: String,
+        val image: String,
+    )
+}
