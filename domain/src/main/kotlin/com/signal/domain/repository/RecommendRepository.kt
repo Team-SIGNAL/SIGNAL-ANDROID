fd57191fbc1028b972d2e5fea7c8dc922dde43c3
Tmdhoon2 <hks026naver@gmail.com>
2023-11-24 07:22:49 +0900

    ⚡️ :: 추천 상세보기 도메인 모듈 로직 구현

--- domain/src/main/kotlin/com/signal/domain/repository/RecommendRepository.kt
+++ domain/src/main/kotlin/com/signal/domain/repository/RecommendRepository.kt
@@ -1,8 +1,12 @@
 package com.signal.domain.repository
 
+import com.signal.domain.entity.RecommendDetailsEntity
 import com.signal.domain.entity.RecommendsEntity
 import com.signal.domain.enums.Category
+import java.util.UUID
 
 interface RecommendRepository {
     suspend fun fetchRecommends(category: Category): Result<RecommendsEntity>
+
+    suspend fun fetchRecommendDetails(recommendId: UUID): Result<RecommendDetailsEntity>
 }
