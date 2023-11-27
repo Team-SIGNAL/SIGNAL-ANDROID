5f9443fc9f7f37de172050159c70dbf0180a2479
chlgkdms <haeun3882@gmail.com>
2023-11-28 08:27:30 +0900

    ⚡️ :: coin api 추가

--- data/src/main/kotlin/com/signal/data/api/CoinApi.kt
+++ data/src/main/kotlin/com/signal/data/api/CoinApi.kt
@@ -1,6 +1,7 @@
 package com.signal.data.api
 
 import com.signal.data.model.coin.CreateCoinRequest
+import com.signal.data.model.coin.CreateCoinResponse
 import com.signal.data.model.coin.FetchCoinsResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,7 +11,7 @@ interface CoinApi {
     @POST(SignalUrl.Coin.CreateCoin)
     suspend fun createCoin(
         @Body createCoinRequest: CreateCoinRequest,
-    )
+    ): CreateCoinResponse
 
     @GET(SignalUrl.Coin.FetchCoins)
     suspend fun fetchCoins(): FetchCoinsResponse
